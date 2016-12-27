51e885bb486b0c89b019a86aeee5e3abca70c326
Yukirin <skeith@users.noreply.github.com>
2016-12-27 11:58:02 +0800

    Fix user = None

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -9,7 +9,7 @@ class Avatar:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def avatar(self, ctx, user: discord.Member):
+    async def avatar(self, ctx, *, user: discord.Member=None):
         """Get user's avatar URL.
 
         THAT SIMPLE!"""
